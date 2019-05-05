ce2752006ebb6753f2c5c95f1882fa1e1ef57d91
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-05-05 16:36:08 +0700

    [ADD] support for /jpcapp

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const jpcapp = express();
 
-app.use(express.static('public'));
+jpcapp.use(express.static('public'));
 
-app.get("/jpcapp", (req, res) => {
+jpcapp.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "comingsoon.html"));
 });
+
+app.use("/jpcapp", jpcapp);
 
 app.listen(3000, console.log("Server is up on port 3000"));
