0ec1f8208e67ac44fbb5c71f87b88c0211d27660
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-05-05 16:52:18 +0700

    [UPDATE] delete subapplication

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const jpcapp = express();
 
-jpcapp.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
 
-jpcapp.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "comingsoon.html"));
 });
 
-app.use("/jpcapp", jpcapp);
+app.get("*", (req, res) => {
+    res.redirect("/");
+})
 
 app.listen(3000, console.log("Server is up on port 3000"));
