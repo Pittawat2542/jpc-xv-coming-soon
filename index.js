26b0b9612dab02f46416b0baa9d5960223ee629d
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-05-05 12:51:54 +0700

    [FIX] wrong url pattern

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+app.get("/่jpcapp", (req, res) => {
     res.sendFile(path.join(__dirname, "comingsoon.html"));
 });
 
