1687e5048406e05a45f550091b3421470e5c340a
Pittawat <11158905+Pittawat2542@users.noreply.github.com>
2019-05-05 12:41:36 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "comingsoon.html"));
+});
+
+app.listen(3000, console.log("Server is up on port 3000"));
